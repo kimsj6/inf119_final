02b235dec75fe4939fab18d527539fd8c032dbd2
kimsj6 <kimsj6@uci.edu>
2025-12-01 16:31:32 -0800

    setup workflow for client.py. Need to add prompts and work on server

--- client.py
+++ client.py
@@ -5,29 +5,69 @@ from langgraph.prebuilt import create_react_agent
 from langchain_google_genai import ChatGoogleGenerativeAI
 import asyncio
 
-model = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0)
-
-server_params = StdioServerParameters(
-    command="python",
-    # Make sure to update to the full absolute path to your math_server.py file
-    args=["server.py"],
+#Agent 1: Requirements analyzer
+requirements_model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0,
 )
 
-async def run_agent():
+#Agent 2: Code generator
+code_model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0,
+)
+
+#Agent 3: Tester / debugger
+test_model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0,
+)
+
+async def run_requirements_agent(raw_description: str, session: ClientSession) -> str:
+    tools = await load_mcp_tools(session)
+
+    # Create and run the agent
+    agent = create_react_agent(requirements_model, tools)
+    agent_response = await agent.ainvoke({"messages": "#Add message here"})
+    return agent_response
+
+async def run_code_agent(requirements: str, session: ClientSession):
+    tools = await load_mcp_tools(session)
+
+    # Create and run the agent
+    agent = create_react_agent(code_model, tools)
+    agent_response = await agent.ainvoke({"messages": "#Add message here"})
+    return agent_response
+
+async def run_test_agent(requirements: str, session: ClientSession):
+    tools = await load_mcp_tools(session)
+
+    # Create and run the agent
+    agent = create_react_agent(test_model, tools)
+    agent_response = await agent.ainvoke({"messages": "#Add message here"})
+    return agent_response
+
+async def main() -> None:
+    raw_description = """
+    """
+
+    server_params = StdioServerParameters(
+    command="python",
+    args=["server.py"],
+    )
+
     async with stdio_client(server_params) as (read, write):
         async with ClientSession(read, write) as session:
-            # Initialize the connection
             await session.initialize()
 
-            # Get tools
-            tools = await load_mcp_tools(session)
+            requirements = await run_requirements_agent(raw_description, session)
 
-            # Create and run the agent
-            agent = create_react_agent(model, tools)
-            agent_response = await agent.ainvoke({"messages": "#Add message here"})
-            return agent_response
+            await run_code_agent(requirements, session)
+
+            await run_test_agent(requirements, session)
+
+
 
 # Run the async function
 if __name__ == "__main__":
-    result = asyncio.run(run_agent())
-    print(result)
+    asyncio.run(main())
